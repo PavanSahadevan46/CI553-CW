81937a4059316e722d310b56d6e2ea4e6f9e77f6
PavanSahadevan46 <pavansahadevan46@gmail.com>
2023-11-21 11:23:53 +0000

    updated thinking

--- dbAccess/reservation.java
+++ dbAccess/reservation.java
@@ -2,6 +2,9 @@ package dbAccess;
 
 public class reservation {
     //TODO create a connection to db
+
+
+    //sql count() function returns a count of rows 
     //basic select update statement that increments based on previous row e.g. if no rows exist than assume 1 then for next user +1 so id = 2
     //find a way to connect this to customer view so that it enables the user to actually use this
     //return details about reservation through the customer view pane
