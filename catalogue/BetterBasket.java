1c5530e817bca4a43b847765d0c88f919d4f894e
PavanSahadevan46 <pavansahadevan46@gmail.com>
2024-01-10 22:05:23 +0000

    added additonal javadoc comments to BetterBasket.java

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -13,13 +13,18 @@ public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
   
+  /**
+   * adds a product to the list, updating quantity if the product already exists,
+   * and making sure  the list is sorted after addition
+   *
+   * @param pr The product to be added to the list
+   * @return true if the product was found and updated, otherwise false
+   */
   @Override
   public boolean add( Product pr )
   {                              
     for (Product productExists : this) { //iterate through each product in current list
       if(productExists.getProductNum().equals(pr.getProductNum())){ // check if a product already exists 
-        // System.out.println(pr);
-        // System.out.println("test2");
         productExists.setQuantity(pr.getQuantity() + productExists.getQuantity());// if it exists, print the product and update its quantity
         return true;// return true to indicate that the product was found and updated
       }
@@ -29,12 +34,13 @@ public class BetterBasket extends Basket implements Serializable
     basketSort();
     return temp; 
   }
-
- //sort the basket
+  /**
+   * sorts the list of products using the product numbers in ascending order
+   * uses the lambda operator to compare item 1 and item 2 in the basket for sorting
+   */
   public void basketSort(){
     //uses lambda operator to compare item 1 and item 2 in the basket then sort it
     Collections.sort(this, (i1, i2) -> i1.getProductNum().compareTo(i2.getProductNum())); 
-     
   }
   
 }
