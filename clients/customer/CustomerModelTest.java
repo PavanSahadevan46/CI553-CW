03da19a891071de5d20988d3a412625e109fc7f7
PavanSahadevan46 <pavansahadevan46@gmail.com>
2024-01-11 11:21:29 +0000

    updated customerModelTest with comments and fixed wrong interface call

--- clients/customer/CustomerModelTest.java
+++ clients/customer/CustomerModelTest.java
@@ -2,7 +2,7 @@ package clients.customer;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import dbAccess.reserveRW;
+import dbAccess.reserveR;
 import middle.LocalMiddleFactory;
 import middle.reserveException;
 
@@ -10,20 +10,20 @@ class CustomerModelTest {
 
 	@Test
 	final void testDoReserve() throws reserveException {
-        CustomerModel customerModel = new CustomerModel(new LocalMiddleFactory());
-        reserveRW ReserveRW;
+        CustomerModel customerModel = new CustomerModel(new LocalMiddleFactory()); //  creating middlefactory to instantiate objects
+        reserveR ReserveR; // need to connect to the database
         
-        int expected = 0;
-        int result = 0;
+        int expected = 0; 
+        int result = 0; 
         try {
-            ReserveRW = new reserveRW();
-            expected = ReserveRW.checkReservationCount();
-            customerModel.doReserve();
-            result = ReserveRW.checkReservationCount();
+            ReserveR = new reserveR(); // creating new instance of reserveR to get access to database
+            expected = ReserveR.checkReservationCount(); // call the method to count the records
+            customerModel.doReserve(); // perform a reservation
+            result = ReserveR.checkReservationCount(); //  call the method to count the records
         } catch (Exception e) {
             System.out.println("Error whilst checking count: " + e);
         }
-        assertEquals(expected , result);
+        assertEquals(expected , result); //result should be equal to expected as the reservation is an autoincrement 
 
         if (expected != result) {
             fail("Expected count to be identical to id number");
